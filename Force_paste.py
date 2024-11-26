78d07b4fc604e621c6b45235f1c1575fac22c5da
Yukun Tian <213221787@seu.edu.cn>
2024-11-26 20:31:44 +0800

    update force_paste

---
+++ Force_paste.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+import pyperclip
+
+
+def type_Eng(text):
+    print("Waiting for 2 seconds...")
+    time.sleep(2)
+    print("Starting to type text...")
+    pyautogui.typewrite(text, interval=0.1)
+    print("Typing completed")
+
+def type_text(text):
+    print("等待2秒...")
+    time.sleep(2)
+    print("开始输入文本...")
+    pyperclip.copy(text)
+    pyautogui.hotkey('ctrl', 'v')
+    print("输入完成")
+
+if __name__ == "__main__":
+    text_to_type = "这是要输入的文本"
+    type_text(text_to_type)
+
+    # text_to_type = "This is the target."
+    # type_Eng(text_to_type)
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Useful-Tools
+## Useful-Tools
 Handy tools and scripts for personal use. 自用的好用小工具和脚本呀！
+
+## Get Started
+
+## Introduction
+
+### force_paste
+
+The `force_paste.py` script allows you to simulate keyboard input for both English and Chinese text. This can be particularly useful in situations where pasting text is not allowed or possible.
+
+#### Functions
+
+- `type_Eng(text)`: Simulates typing English text .
+- `type_text(text)`: Simulates typing Chinese text.
+
